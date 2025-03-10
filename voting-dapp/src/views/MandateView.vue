d8b0881dff7c2b49f2e30079b0b20de2b658ef7f
heize777 <cooljoe10088@gmail.com>
2025-03-10 15:06:54 +0800

    Update MandateView.vue

--- voting-dapp/src/views/MandateView.vue
+++ voting-dapp/src/views/MandateView.vue
@@ -6,7 +6,6 @@ const web3Obj = ref(null)
 
 const host = ref("0x0000000000000000000000000000000000000000")
 
-// ["0xb047CC1dB5b719A3696C74dc76E0BE968A0BA924" ,"0x11C74478470262F5506d1F471b253031157B0652" ,"0xfBf5872BdCAAE6d65802B3858d945EeDCba53DB3"]
 const voterAddress = ref("")
 const getHost = async () => {
   host.value = await web3Obj.value.voteContract.methods.host().call()
